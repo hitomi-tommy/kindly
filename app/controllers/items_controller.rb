504374e2f3b0305fb332fb4472ff18b150777eeb
hitomi kanaya <hitomickeykey@gmail.com>
2020-10-16 21:55:03 +0900

    itemsのshow画面作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,10 @@ class ItemsController < ApplicationController
     redirect_to items_path
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :images, :budget, :deadline, :status)
