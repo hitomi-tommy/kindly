29cb726b45c9fc8657c2f6ed77eaaa64f10919e9
hitomi kanaya <hitomickeykey@gmail.com>
2020-10-24 01:31:56 +0900

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kindly_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'http://damp-ridge-65975.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://stark-hamlet-04862.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
