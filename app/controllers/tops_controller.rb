48018cd910c70307ebc00b455054522da25bdcf8
hitomi kanaya <hitomickeykey@gmail.com>
2020-10-27 19:31:51 +0900

    トップページの表示を降順に変更

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,6 @@ class TopsController < ApplicationController
   def index
     @items = Item.all
     @q = Item.ransack(params[:q])
-    @items = @q.result
+    @items = @q.result.order(created_at: :DESC)
   end
 end
