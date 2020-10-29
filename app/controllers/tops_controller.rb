15d79453e8bfbbc84458a544a32e574a5c30aa43
hitomi kanaya <hitomickeykey@gmail.com>
2020-10-29 16:49:24 +0900

    topページにページネーション追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,6 @@
 class TopsController < ApplicationController
   def index
     @q = Item.ransack(params[:q])
-    @items = @q.result.order(created_at: :DESC)
+    @items = @q.result.order(created_at: :DESC).page(params[:page]).per(20)
   end
 end
